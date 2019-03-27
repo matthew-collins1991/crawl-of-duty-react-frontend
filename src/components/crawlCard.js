faaf7771085dd01afdfa9a959f04b79fe1655d9c
JoeyHere <josephodell@outlook.com>
2019-03-27 16:00:13 +0000

    1

--- src/components/crawlCard.js
+++ src/components/crawlCard.js
@@ -13,11 +13,11 @@ class CrawlCard extends React.Component {
           <h3 className="header">{this.props.name}</h3>
           <small>{this.props.location.toUpperCase()}</small>
           <ol>
-            {this.props.pubs.slice(0, 6).map(pub => (
+            {this.props.pubs.slice(0, 5).map(pub => (
               <li key={pub.id}>{pub.name}</li>
             ))}
           </ol>
-          {this.props.pubs.length > 6 ? <p>more...</p> : null}
+          {this.props.pubs.length > 5 ? <p>more...</p> : null}
         </div>
       </Card>
     )
